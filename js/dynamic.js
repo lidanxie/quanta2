263f08a3abf1fbbcd569a3f368df48f558e9ce1b
Lidan Xie <987905457@qq.com>
2016-09-01 14:49:11 +0800

    test

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,24 +1,125 @@
 // 动态列表页面的js
 $(document).ready(function(){
 	// 1.将数据库中的信息分页展示出来
-	$.ajax({
-		type:"POST",
-		url:"",//后台的文件
-		dataType:"json",   //json文件类型
-		success:function(data){
-			$(".all_msg").empty();
-			var $html='';
-	        $.each(_jsonDta,function(itemIndex,item){
-		        $html+='<div class="item"><div class="time">'+item['time']+'</div><a href=\"'+item['msgUrl']+'\"class="msg_til"  target="_blank">'+item['msgTil']+'</a><a href=\"'+item['msgUrl']+'\" class="see"  target="_blank">查看</a><a href="javascript" class="delete">删除</a></div>'
-	        });
-	        $("#all_msg").append($html);
-		},
-		error:function(data){
-			$(".all_msg").empty();
-			var $error=$('<div class="error">数据库加载出错啦！</div>')
-			$(".all_msg").append()
-		}
-	});  //end ajax
+	// $.ajax({
+	// 	type:"POST",
+	// 	url:"",//后台的文件
+	// 	dataType:"json",   //json文件类型
+	// 	success:function(data){
+	// 		$(".all_msg").empty();
+	// 		var $html='';
+	//         $.each(_jsonDta,function(itemIndex,item){
+	// 	        $html+='<div class="item"><div class="time">'+item['time']+'</div><a href=\"'+item['msgUrl']+'\"class="msg_til"  target="_blank">'+item['msgTil']+'</a><a href=\"'+item['msgUrl']+'\" class="see"  target="_blank">查看</a><a href="javascript" class="delete">删除</a></div>'
+	//         });
+	//         $("#all_msg").append($html);
+	// 	},
+	// 	error:function(data){
+	// 		$(".all_msg").empty();
+	// 		var $error=$('<div class="error">数据库加载出错啦！</div>')
+	// 		$(".all_msg").append()
+	// 	}
+	// });  //end ajax
+
+// 测试数据
+var _jsonData=[{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-2",
+    "msgUrl":"see_msg2.html",
+    "msgTil":"quanta招新2",
+},{
+    "time":"2016-8-3",
+    "msgUrl":"see_msg3.html",
+    "msgTil":"quanta招新3",
+},{
+    "time":"2016-8-4",
+    "msgUrl":"see_msg4.html",
+    "msgTil":"quanta招新4",
+},
+{
+    "time":"2016-8-5",
+    "msgUrl":"see_msg5.html",
+    "msgTil":"quanta招新5",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},{
+    "time":"2016-8-1",
+    "msgUrl":"see_msg1.html",
+    "msgTil":"quanta招新1",
+},];
+
 
 	// 封装分页
 	var pageTotal={
@@ -60,9 +161,39 @@ $(document).ready(function(){
                         pageTotal.page(type); //type表示分页栏样式
                 },   //end getDate
                 page :function(type){    //分页的样式设置
+                        //清除分页栏元素
+                        $("#pages").empty();
+                        //填充分页样式欠要加载分页数据
+                        pageTotal.getPages();
+                        if( type == 1 ){
+                            var x = 3;
+                             //设置上下页
+                            $("#pages").append("记录&nbsp;&nbsp;&nbsp;&nbsp"+pageTotal.count+"&nbsp;&nbsp;条&nbsp;&nbsp;&nbsp");
+                            $("#pages").append("页数&nbsp;&nbsp;&nbsp;&nbsp"+pageTotal.current+"/"+pageTotal.total+"&nbsp;&nbsp;页&nbsp;&nbsp;&nbsp;<a href='javascript:pageTotal.getDate(1,"+type+");'>首页</a>&nbsp;&nbsp;&nbsp;&nbsp;");
+                            var index = pageTotal.current <=Math.ceil(x/2)?1:(pageTotal.current)>=pageTotal.total-Math.ceil(x/2)?pageTotal.total-x:(pageTotal.current-  Math.ceil(x/2));
 
-                }
+                            var end = pageTotal.current <= Math.ceil(x/2)?(x+1):(pageTotal.current+Math.ceil(x/2))>=pageTotal.total?pageTotal.total:(pageTotal.current+Math.ceil(x/2));
 
-	};   //end pageTotal   
+                            for(var i = index ; i <= end ; i++){
+                                    if(i == pageTotal.current){
+                                            $("#pages").append("<a href='javascript:pageTotal.getDate("+pageTotal.current+","+type+");' class='on'>"+i+"</a>");
+                                    }else{
+                                            $("#pages").append("<a href='javascript:pageTotal.getDate("+i+","+type+");'>"+i+"</a>");
+                                    }
+                            } //end for
+
+                            if(end != pageTotal.total){
+                                    $("#pages").append("&nbsp;&nbsp;&nbsp;&nbsp;<span>...</span>");
+                            } //end if
+                            $("#pages").append("&nbsp;&nbsp;&nbsp;&nbsp;<a href='javascript:pageTotal.getDate("+pageTotal.last+","+type+"); '>尾页</a>");
+                            
+                        }  //end if
+                },  //end page
+
+	};   //end pageTotal  
+    $(function(){
+pageTotal.getDate(1,1);
+});
+ 
 
 });//end document
